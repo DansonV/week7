1a7877d3a864a5188192c0dac74204208fff2daa
DansonVuong <70728975+DansonVuong@users.noreply.github.com>
2020-10-19 16:34:06 -0500

    Add files via upload

---
+++ human.js
@@ -0,0 +1,8 @@
+var human = {
+	name: "Finn the Human",
+	hp: 100,
+	damage: 15,
+	sml_potion_num: 1,
+	med_potion_num: 0,
+	lrg_potion_num: 0
+}
---
+++ monsters.js
@@ -0,0 +1,20 @@
+var listOfMonsters= [
+			{
+						name: "Slime",
+						hp: 15,
+						damage: 8,
+						image: "https://webstockreview.net/images/monster-clipart-slimy.png"
+			},
+			{
+						name: "Zombie",
+						hp: 45,
+						damage: 12,
+						image: "https://clipground.com/images/zombie-clipart-9.jpg"
+			},
+			{
+						name: "Orc",
+						hp: 75,
+						damage: 20,
+						image: "https://philgamer.files.wordpress.com/2009/09/orc.jpg?w=614"
+			}	
+]
